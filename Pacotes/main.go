0cf007083964d9bb31d5e45d8a039669d6007cb0
Arthur Romani <arthur.inacio.28@gmail.com>
2025-09-21 09:14:46 -0300

    pacotes externos

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -4,9 +4,13 @@ import
 (
 	"modulo/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Arquivo main")
 	auxiliar.Escrever()
+
+	erro:= checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro) 
 }
